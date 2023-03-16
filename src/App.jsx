e13d59cd1942e0dcdf0b36ba1d3f4ed98d9d116c
rcseven <rcedquila@gmail.com>
2023-03-16 14:58:51 +0800

    Add Caesar Cipher UI and Logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./styles.scss";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputText, setInputText] = useState("");
+  const [inputKey, setInputKey] = useState(0);
+  const [outputText, setOutputText] = useState(null);
+
+  function handleEncrypt(e) {
+    e.preventDefault();
+    setOutputText(caesarCipher(inputText, inputKey, "encrypt"));
+  }
+
+  function handleDecrypt(e) {
+    e.preventDefault();
+    setOutputText(caesarCipher(inputText, inputKey, "decrypt"));
+  }
+
+  function caesarCipher(str, shift, action) {
+    let result = "";
+
+    if (action === "encrypt") {
+      shift = shift % 26;
+    } else if (action === "decrypt") {
+      shift = (26 - shift) % 26; //
+    } else {
+      throw new Error("Invalid action: " + action);
+    }
+
+    for (let i = 0; i < str.length; i++) {
+      let char = str[i];
+      let charCode = str.charCodeAt(i);
+
+      if (
+        (charCode >= 65 && charCode <= 90) ||
+        (charCode >= 97 && charCode <= 122)
+      ) {
+        let offset = charCode >= 97 ? 97 : 65;
+
+        charCode = ((charCode - offset + shift) % 26) + offset;
+        char = String.fromCharCode(charCode);
+      }
+
+      result += char;
+    }
+
+    return result;
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+    <div className="container">
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <h1>Caesar's Cipher</h1>
+        <div className="monkey"></div>
+        <form>
+          <div className="form-input">
+            <input
+              type="text"
+              placeholder="Text"
+              onChange={(e) => setInputText(e.target.value)}
+            />
+            <input
+              type="number"
+              placeholder="Key"
+              onChange={(e) => setInputKey(e.target.value)}
+            />
+          </div>
+          <div className="form-btn">
+            <button onClick={handleEncrypt}>Encrypt</button>
+            <button onClick={handleDecrypt}>Decrypt</button>
+          </div>
+        </form>
+        <p>{outputText}</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
